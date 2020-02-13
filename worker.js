ee59e1fd7ec7495da8f8c30a203f4acfcb745ca6
zwyak <vlad.senyuk97@gmail.com>
2020-02-13 16:06:09 +0300

    Added worker.js, added writeFile and setTimeout

---
+++ worker.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const file = process.argv[2];
+const x = process.argv[3];
+
+fs.writeFile(file, '', (err) => {
+  if (err) throw err;
+  console.log('The file has been created!');
+});
+
+function writeFile(){
+  const number = Date.now();
+  const json = JSON.stringify({number:number});
+
+  fs.appendFile(file, json, (err) => {
+    if (err) throw err;
+    console.log('The "data to append" was appended to file!');
+  });
+}
+
+setTimeout(writeFile, x);
