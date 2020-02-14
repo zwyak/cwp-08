716dca1b8c815da481dc79eeb18d3c745bb103e1
zwyak <vlad.senyuk97@gmail.com>
2020-02-14 09:59:54 +0300

    Updated tcp-server

--- tcp-server.js
+++ tcp-server.js
@@ -1,9 +1,11 @@
 // tcp-server.js
 const net = require('net');
 const path = require('path');
-const { execFile } = require('child_process');
+const spawn = require('child_process').spawn;
 
 const port = 8124;
+
+let workers = [];
 
 const server = net.createServer((client) => {
   console.log('Client connected');
@@ -11,7 +13,40 @@ const server = net.createServer((client) => {
   client.setEncoding('utf8');
 
   client.on('data', (data) => {
-    console.log(data);
+    const req = data.split(' ');
+    console.log(`Request: ${data}`);
+
+    switch (req[0]) {
+      case '/process/create':
+        const fileName = Date.now().toSting();
+        const child = spawn(`node ${path.join(req[1], fileName)} ${req[2]}`, {detached:true});
+        workers.push(child);
+        client.write(`Process ${child.pid} was started`);
+        break;
+      case '/process/list':
+        let processList = '';
+        workers.forEach((item, i) => {
+          processList += item.pid.toString() + '\r\n'
+        });
+        client.write(processList);
+        break;
+      case '/process/kill':
+        let flag = false;
+        workers.forEach((item, i) => {
+          if (item.pid == req[1]) flag = true;
+        });
+        if (flag == false){
+          client.write(`Process ${req[1]} wasn't found`);
+          break;
+        }
+        process.kill(req[1]);
+        client.write(`Process ${req[1]} was stoped`);
+        break;
+      default:
+        client.write('Bad Request');
+        client.destroy();
+        break;
+    }
     client.write('\r\nHello!\r\nRegards,\r\nServer\r\n');
   });
 
